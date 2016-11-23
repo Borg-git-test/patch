9c0f5cfea66fbac58c6a1c8325d57e01ea3a3a33
Andrii Zahura <shkiper@2261152.no-reply.drupal.org>
2016-11-23 07:39:29 +0200

    update selectors in JS according to changes in Drupal 8 form structure

--- modules/field_tooltips_data/js/tooltips-form.js
+++ modules/field_tooltips_data/js/tooltips-form.js
@@ -3,73 +3,75 @@
  * Tooltips builder on the node form.
  */
 
-(function ($, Drupal) {
+(function ($, Drupal, drupalSettings) {
   Drupal.behaviors.tooltipsAutocomplete = {
     attach: function (context, settings) {
-      var $tooltipsContainer = $('.paragraphs-item-type-image-tooltips');
-      var autocompleteField = '.field-name-field-tooltips-content input';
+      var $tooltipsContainer = $('.paragraph-image-tooltips');
+      var autocompleteField = '.field--name-field-tooltips-content input';
 
       // Add tooltips for the node form on page load.
-      $tooltipsContainer.find('.field-name-field-tooltips-data').not('.tooltips-processed').each(function() {
+      $tooltipsContainer.find('.field--name-field-tooltips-data').not('.tooltips-processed').each(function() {
         var $this = $(this);
         var tooltisValue = $this.find('input').val();
         if (tooltisValue) {
           var tooltipsData = JSON.parse(tooltisValue);
-          var $baseImage = $this.siblings('.field-name-field-tooltip-base-image');
+          var $baseImage = $this.siblings('.field--name-field-tooltip-base-image');
           var tooltip, configLink;
           $.each(tooltipsData, function(index, icon) {
-            tooltip = Drupal.theme('image_tooltip_icon_form', icon);
+            tooltip = Drupal.theme('imageTooltipIconForm', icon);
             $(tooltip).appendTo($baseImage).draggable();
 
-            configLink = Drupal.theme('tooltip_config_link', icon.nid);
-            $(configLink).insertAfter($this.siblings('.field-name-field-tooltips-content').find('input[id~="' + icon.delta + '"]'));
+            configLink = Drupal.theme('tooltipConfigLink', icon.nid);
+            $(configLink).insertAfter($this.siblings('.field--name-field-tooltips-content').find('input[id~="' + icon.delta + '"]'));
           });
         }
         $this.addClass('tooltips-processed');
       });
 
       // Add tooltip on image for the selected node in autocomplete.
-      $(autocompleteField).once('add-tooltip').on('autocompleteSelect', function(event, node) {
-        var $this = $(this);
-        var $baseImage = $this.parents('.field-name-field-tooltips-content').siblings('.field-name-field-tooltip-base-image');
+      $(autocompleteField).autocomplete({
+        select: function (event, ui) {
+          var $this = $(this);
+          var $baseImage = $this.parents('.field--name-field-tooltips-content').siblings('.field--name-field-tooltip-base-image');
 
-        var iconImageSrc = $this.parents('.field-name-field-tooltips-content').siblings('.field-name-field-tooltip-icon').find('a').attr('href');
+          var iconImageSrc = $this.parents('.field--name-field-tooltips-content').siblings('.field--name-field-tooltip-icon').find('a').attr('href');
 
-        if (!iconImageSrc) {
-          iconImageSrc = Drupal.settings.imageTooltip.icon;
+          if (!iconImageSrc) {
+            iconImageSrc = drupalSettings.imageTooltip.icon;
+          }
+          var selectedNode = ui.item.value;
+
+          var selectedMatches = selectedNode.match('\(([1-9])+\)');
+          var selectedNid = selectedMatches[0];
+          var selectedtitle = ui.item.label;
+
+          var icon = {
+            nid: selectedNid,
+            title: selectedtitle,
+            src: iconImageSrc,
+            delta: $this.data('drupal-selector')
+          };
+
+          var tooltip = Drupal.theme('imageTooltipIconForm', icon);
+          $(tooltip).appendTo($baseImage).draggable();
+
+          var editTooltipPosition = Drupal.theme('tooltipConfigLink', selectedNid);
+          $(editTooltipPosition).insertAfter($this);
         }
-        var selectedNode = $this[0].value;
-
-        var selectedMatches = selectedNode.match('\(([1-9])+\)');
-        var selectedNid = selectedMatches[0];
-        var selectedtitle = selectedNode.replace(/\([0-9]\)/g, '').trim();
-
-        var icon = {
-          nid: selectedNid,
-          title: selectedtitle,
-          src: iconImageSrc,
-          delta: $this.attr('id')
-        };
-
-        var tooltip = Drupal.theme('image_tooltip_icon_form', icon);
-        $(tooltip).appendTo($baseImage).draggable();
-
-        var editTooltipPosition = Drupal.theme('tooltip_config_link', selectedNid);
-        $(editTooltipPosition).insertAfter($this);
       });
 
       // Remove saved tooltip if user ties to change field.
       $(autocompleteField).bind('focus', function(event, node) {
         var $this = $(this);
 
-        var $baseImage = $this.parents('.field-name-field-tooltips-content').siblings('.field-name-field-tooltip-base-image');
+        var $baseImage = $this.parents('.field--name-field-tooltips-content').siblings('.field--name-field-tooltip-base-image');
 
         // Add warning if user has not uploaded base image before selecting tooltips.
         if (!$baseImage.find('.image-preview').length || !$baseImage.find('img').attr('src')) {
-          var $container = $this.parents('.paragraphs-item-type-image-tooltips');
+          var $container = $this.parents('.paragraph-image-tooltips');
           if (!$container.find('.tooltips-empty-warning').length) {
-            var warningMessage = Drupal.theme('tooltip_warning');
-            $(warningMessage).insertBefore($this.parents('.field-name-field-tooltips-content'));
+            var warningMessage = Drupal.theme('tooltipWarning');
+            $(warningMessage).insertBefore($this.parents('.field--name-field-tooltips-content'));
             $this.blur();
           }
 
@@ -80,7 +82,7 @@
         $baseImage.find('.tip[data-delta="' + $this.attr('id') + '"]').remove();
         $this.siblings('.tooltip-config-link').remove();
 
-        var $savedPositionsInput = $this.parents('.field-name-field-tooltips-content').siblings('.field-name-field-tooltips-data input');
+        var $savedPositionsInput = $this.parents('.field--name-field-tooltips-content').siblings('.field--name-field-tooltips-data input');
 
         var savedData = $savedPositionsInput.val();
         if (savedData) {
@@ -93,7 +95,7 @@
       });
 
       // Save tooltip position in JSON.
-      $('.field-name-field-tooltip-base-image', context).once('change-position').on('dragstop',function(ev, ui) {
+      $('.field--name-field-tooltip-base-image', context).once('change-position').on('dragstop',function(ev, ui) {
         var $this = $(this);
         var icon = {
           nid: $(ev.target).data('nid'),
@@ -103,7 +105,7 @@
           left: ui.position.left,
           top: ui.position.top
         };
-        var $savedPositionsField = $this.siblings('.field-name-field-tooltips-data');
+        var $savedPositionsField = $this.siblings('.field--name-field-tooltips-data');
 
         var savedData = $savedPositionsField.find('input').val();
         var savedJson;
@@ -123,7 +125,7 @@
       // Highlight active tooltip.
       $tooltipsContainer.on('click', '.tooltip-configure-link', function(event, node) {
         var $this = $(this);
-        var $conatiner = $this.closest('.paragraphs-item-type-image-tooltips');
+        var $conatiner = $this.closest('.paragraph-image-tooltips');
         $conatiner.find('.tip').removeClass('active');
         var nid = $this.data('nid');
         $conatiner.find('.tip[data-nid="' + nid + '"]').addClass('active');
@@ -137,16 +139,16 @@
 
       // Add tooltips links that were removed during form rebuild.
       $(document).ajaxComplete(function() {
-        $tooltipsContainer.find('.field-name-field-tooltips-data').each(function() {
+        $tooltipsContainer.find('.field--name-field-tooltips-data').each(function() {
           var $this = $(this);
-          var $tooltipsContentField = $this.siblings('.field-name-field-tooltips-content');
+          var $tooltipsContentField = $this.siblings('.field--name-field-tooltips-content');
           if (!$tooltipsContentField.find('.tooltip-config-link').length) {
             var tooltisValue = $this.find('input').val();
             if (tooltisValue) {
               var tooltipsData = JSON.parse(tooltisValue);
               var configLink;
               $.each(tooltipsData, function(index, icon) {
-                configLink = Drupal.theme('tooltip_config_link', icon.nid);
+                configLink = Drupal.theme('tooltipConfigLink', icon.nid);
                 $(configLink).insertAfter($tooltipsContentField.find('input[id^="' + icon.delta + '"].form-text'));
               });
             }
@@ -156,4 +158,4 @@
 
     }
   }
-})(jQuery, Drupal);
+})(jQuery, Drupal, drupalSettings);
