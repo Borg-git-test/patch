8a00b2fb6975988bcb9059da440fa2ab86170a34
Andrii Zahura <shkiper@2261152.no-reply.drupal.org>
2016-11-23 07:35:50 +0200

    update templates for Drupal 8 standards

--- modules/field_tooltips_data/js/templates.js
+++ modules/field_tooltips_data/js/templates.js
@@ -3,9 +3,9 @@
  * Templates for tooltips.
  */
 
-(function ($) {
+(function ($, Drupal, drupalSettings) {
 
-  Drupal.theme.prototype.image_tooltip_icon_form = function(icon) {
+  Drupal.theme.imageTooltipIconForm = function(icon) {
     var html = '';
     html += '<div class="tip ui-draggable" id="tooltip-icon-' + icon.nid + '" data-nid="' + icon.nid + '" data-title="' + icon.title + '" data-src="' + icon.src + '" data-delta="' + icon.delta + '" style="position: relative; left: ' + icon.left + 'px; top: ' + icon.top + 'px;">';
     html += '<img class="tooltip-icon" src="' + icon.src + '" title="' + icon.title + '">';
@@ -13,17 +13,17 @@
     return html;
   };
 
-  Drupal.theme.prototype.image_tooltip_icon_view = function(icon) {
+  Drupal.theme.imageTooltipIconView = function(icon) {
     var html = '';
     html += '<div class="tip" data-nid="' + icon.nid + '" style="position: relative; left: ' + icon.left + 'px; top: ' + icon.top + 'px;">';
-    html += '<a href="/tooltip/' + icon.nid + '/nojs" class="ctools-use-modal ctools-modal-tooltip" title="' + icon.title + '">';
+    html += '<a href="/tooltip/' + icon.nid + '/nojs" class="use-ajax" data-dialog-type="modal" title="' + icon.title + '">';
     html += '<img class="tooltip-icon" id="tooltip-icon-' + icon.nid + '" src="' + icon.src + '">';
     html += '</a>';
     html += '</div>';
     return html;
   };
 
-  Drupal.theme.prototype.tooltip_config_link = function(nid) {
+  Drupal.theme.tooltipConfigLink = function(nid) {
     var html = '';
     html += '<div class="tooltip-config-link">';
     html += '<a href="#tooltip-icon-' + nid + '" class="tooltip-configure-link" data-nid="' + nid + '">';
@@ -33,7 +33,7 @@
     return html;
   };
 
-  Drupal.theme.prototype.tooltip_warning = function() {
+  Drupal.theme.tooltipWarning = function() {
     var html = '';
     html += '<div class="tooltips-empty-warning">';
     html += '<span class="closebtn">&times;</span>';
@@ -43,4 +43,4 @@
     return html;
   };
 
-})(jQuery);
+})(jQuery, Drupal, drupalSettings);
